9f667e5928c38ca00a484694bf6ee7c01244f32c
Neeraj Dalal <admin@nrjdalal.com>
2025-11-27 05:55:06 +0530

    chore: leverage mono

--- web/next/src/lib/api/client.ts
+++ web/next/src/lib/api/client.ts
@@ -1,7 +1,11 @@
 import type { AppType } from "@api/hono"
 import { hc } from "hono/client"
 
-export const honoClient = hc<AppType>(process.env.NEXT_PUBLIC_API_URL as string, {
+type Client = ReturnType<typeof hc<AppType>>
+
+const hcWithType = (...args: Parameters<typeof hc>): Client => hc<AppType>(...args)
+
+export const honoClient = hcWithType(process.env.NEXT_PUBLIC_API_URL as string, {
   init: {
     credentials: "include",
   },
