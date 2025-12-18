863748fed38f689b6521f3ea86886090d04b80d8
Neeraj Dalal <admin@nrjdalal.com>
2025-12-19 05:08:38 +0530

    chore: v1

--- web/next/src/app/docs/layout.tsx
+++ web/next/src/app/docs/layout.tsx
@@ -7,7 +7,13 @@ import { source } from "@/lib/source"
 export default function Layout({ children }: LayoutProps<"/docs">) {
   return (
     <RootProvider>
-      <DocsLayout tree={source.pageTree} {...baseOptions()}>
+      <DocsLayout
+        sidebar={{
+          enabled: false,
+        }}
+        tree={source.pageTree}
+        {...baseOptions()}
+      >
         {children}
       </DocsLayout>
     </RootProvider>
