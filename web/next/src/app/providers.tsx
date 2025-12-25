564641a57ceda2a75c0f852ec2e6fb8b02245b7e
Neeraj Dalal <admin@nrjdalal.com>
2025-12-25 05:39:04 +0530

    chore: hide feedback in development too

--- web/next/src/app/providers.tsx
+++ web/next/src/app/providers.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react"
 import Script from "next/script"
 
-import { isLocal, isProduction } from "@packages/env"
+import { isDevelopment, isLocal, isProduction } from "@packages/env"
 import { env } from "@packages/env/web-next"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
@@ -32,24 +32,26 @@ export function InnerProvider({ children }: { children: React.ReactNode }) {
     >
       {children}
       <Toaster richColors />
-      {!isLocal(process.env.NODE_ENV) && env.NEXT_PUBLIC_USERJOT_ID && (
-        <>
-          <Script
-            id="userjot-sdk"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `window.$ujq=window.$ujq||[];window.uj=window.uj||new Proxy({},{get:(_,p)=>(...a)=>window.$ujq.push([p,...a])});document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://cdn.userjot.com/sdk/v2/uj.js',type:'module',async:!0}));`,
-            }}
-          />
-          <Script
-            id="userjot-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `window.uj.init('${env.NEXT_PUBLIC_USERJOT_ID}', { widget: true, position: 'right', theme: 'auto' });`,
-            }}
-          />
-        </>
-      )}
+      {!isLocal(process.env.NODE_ENV) &&
+        !isDevelopment(process.env.NODE_ENV) &&
+        env.NEXT_PUBLIC_USERJOT_ID && (
+          <>
+            <Script
+              id="userjot-sdk"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `window.$ujq=window.$ujq||[];window.uj=window.uj||new Proxy({},{get:(_,p)=>(...a)=>window.$ujq.push([p,...a])});document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://cdn.userjot.com/sdk/v2/uj.js',type:'module',async:!0}));`,
+              }}
+            />
+            <Script
+              id="userjot-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `window.uj.init('${env.NEXT_PUBLIC_USERJOT_ID}', { widget: true, position: 'right', theme: 'auto' });`,
+              }}
+            />
+          </>
+        )}
     </NextThemesProvider>
   )
 }
